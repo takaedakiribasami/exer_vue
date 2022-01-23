82fc5b9909404127a5dfa8369ceef3b3bb990381
masashi.takada <b132036@gmail.com>
2022-01-24 00:21:51 +0900

    20220124_単一コンポーネントテスト

---
+++ section6/sfc/bar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはBarコンポーネントです。</p>
+    <foo class="header" />
+  </div>
+</template>
+
+<script>
+import Foo from "./foo";
+export default {
+  components: {
+    Foo,
+  },
+};
+</script>
+
+<style scoped>
+.bar {
+  border: solid 1px red;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 100%;
+}
+</style>
---
+++ section6/sfc/hello.vue
@@ -0,0 +1,17 @@
+<template>
+    <p class="message">メッセージ：{{ msg }}</p>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { msg: "HELLO" }
+        }
+    }
+</script>
+
+<style>
+    .message {
+        color: #42b983
+    }
+</style>
