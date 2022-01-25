117eff9172d8f6c07d1f61f476139795de5e57b2
masashi.takada <b132036@gmail.com>
2022-01-25 13:43:21 +0900

    20220125_ラベル機能追加

--- section7/src/store.js
+++ section7/src/store.js
@@ -1,21 +1,63 @@
 import { createStore } from 'vuex'
 
 const store = createStore({
-    state() {
-        return {
-            tasks: [
-                {
-                    id: 1,
-                    name: '牛乳を買う',
-                    done: false
-                },
-                {
-                    id: 2,
-                    name: 'Vue.jsの本を買う',
-                    done: true
-                },
-            ]
-        }
+    state: {
+        tasks: [
+            {
+                id: 1,
+                name: '牛乳を買う',
+                labelIds: [1, 2],
+                done: false
+            },
+            {
+                id: 2,
+                name: 'Vue.jsの本を買う',
+                labelIds: [1, 3],
+                done: true
+            },
+        ],
+        nextTaskId: 3,
+        labels: [
+            {
+                id: 1,
+                text: '買い物'
+            },
+            {
+                id: 2,
+                text: '食料'
+            },
+            {
+                id: 3,
+                text: '本'
+            },
+        ],
+        nextLabelId: 4,
+    },
+    mutations: {
+        addTask(state, { name, labelIds }) {
+            state.tasks.push({
+                id: state.nextTaskId,
+                name,
+                labelIds,
+                done: false
+            })
+            state.nextTaskId++
+        },
+        toggleTaskStatus(state, { id }) {
+            const filtered = state.tasks.filter(task => {
+                return task.id === id
+            })
+            filtered.forEach(task => {
+                task.done = !task.done
+            })
+        },
+        addLabel(state, { text }) {
+            state.labels.push({
+                id: state.nextLabelId,
+                text
+            })
+            state.nextLabelId++
+        },
     }
 })
 
