0695b2c94d812d0f16a7f4f105df50232136f2e8
masashi.takada <b132036@gmail.com>
2022-01-30 23:42:08 +0900

    20220130_KbnButton単体テスト作成

--- section8/kanban-app/src/store/index.js
+++ section8/kanban-app/src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from "vuex";
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
   modules: {},
+  strict: prpcess.env.NODE_ENV !== 'production'
 });
